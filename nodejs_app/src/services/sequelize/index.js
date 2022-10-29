927e9812e118b7359f07221c60c29317f37e93f1
chandra <cb957146@gmail.com>
2022-10-29 12:04:23 +0000

    setup sequlizer

---
+++ nodejs_app/src/services/sequelize/index.js
@@ -0,0 +1,27 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.SK_MYSQLDB_DB_NAME,
+  process.env.SK_MYSQLDB_USERNAME,
+  process.env.SK_MYSQLDB_PASSWORD,
+  {
+    host: process.env.SK_MYSQLDB_HOST,
+    dialect: "mysql",
+    operatorsAliases: 0,
+    logging: false,
+    pool: {
+      max: 500,
+      min: 0,
+      idle: 10000,
+      acquire: 100 * 1000,
+    },
+  }
+);
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+sequelize.sync({ alter: true });
+module.exports = db;
